0301269f9f2e3bf14dd452e1cd68a980dd3f9252
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-16 16:52:33 +0100

    Grafica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,34 @@
 @section('title-page', 'Movies')
 
 @section('main-content')
-    <h1>DB Films</h1>
+    <div class="header">
+        <hr>
+        <h1>DB Films</h1>
+        <hr>
+    </div>
+
     {{-- Records tabella movies del db movies --}}
 
-    @foreach ($movies as $elem)
-        
-        <h2>{{ $elem['title'] }}</h2>
-
-    @endforeach
+    <div class="container">
+        <div class="row g-3">
+            @foreach ($movies as $elem)
+                <div class="col-3">
+                    <div class="cartaFilm card">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <div>
+                                <h4 class="card-title">{{ $elem['title'] }}</h4>
+                                <h6 class="card-text">- {{ $elem['original_title'] }} -</h6>
+                                <p class="card-text">Nazionalità: {{ $elem['nationality'] }}</p>
+                                <p class="card-text">Data di uscita: {{ $elem['date'] }}</p>
+                                <p class="card-text">Voto: {{ $elem['vote'] }}</p>
+                            </div>
+                            <div>
+                                <a href="#" class="pulsante btn btn-danger">Guarda</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
-
-
