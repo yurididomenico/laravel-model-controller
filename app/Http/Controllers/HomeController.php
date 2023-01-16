ae353669aa9ae6c1f1f8155f04fc33e1ae321a32
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-16 15:24:39 +0100

    Set Controller e Modello

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // 'seclect * from movies'
+        $movie = Movie;
+
+
+
+        return view('welcome');
+    }
+}
